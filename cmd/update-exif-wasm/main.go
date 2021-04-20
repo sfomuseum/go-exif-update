1b3f674911b26f5c8d10cc5d962e28f824b3dac5
thisisaaronland <thisisaaronland@localhost>
2021-04-19 17:43:42 -0700

    still debugging things; but can actually be invoked now

--- cmd/update-exif-wasm/main.go
+++ cmd/update-exif-wasm/main.go
@@ -31,19 +31,25 @@ func UpdateFunc() js.Func {
 
 		b64_data := args[0].String()
 		enc_props := args[1].String()
-		
+
 		handler := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 
 			resolve := args[0]
 			reject := args[1]
 
+			log.Println("WOO", resolve, reject)
+
 			go func() {
 
+				log.Println("GOING...")
+
 				if !b64_pat.MatchString(b64_data) {
+					log.Println("SAD", 1)
 					reject.Invoke("Base64 image data failed match")
 					return
 				}
 
+				log.Println("STEP", 1)
 				m := b64_pat.FindStringSubmatch(b64_data)
 
 				format := m[1]
@@ -51,20 +57,25 @@ func UpdateFunc() js.Func {
 
 				// decode the EXIF properties
 
+				log.Println("STEP", 2)
+
 				var exif_data map[string]interface{}
 
 				err := json.Unmarshal([]byte(enc_props), &exif_data)
 
 				if err != nil {
+					log.Println("SAD", 2)
 					reject.Invoke(fmt.Sprintf("Failed to unmarshal properties, %v", err))
 					return
 				}
 
+				log.Println("STEP", 3)
 				// decode the images
 
 				b64_fh := strings.NewReader(b64_img)
 				var img_fh io.Reader
 
+				log.Println("STEP", 4)
 				if format == "jpeg" {
 					img_fh = base64.NewDecoder(base64.StdEncoding, b64_fh)
 				} else {
@@ -74,6 +85,7 @@ func UpdateFunc() js.Func {
 					im, _, err := image.Decode(tmp_fh)
 
 					if err != nil {
+						log.Println("SAD", 3)
 						reject.Invoke(fmt.Sprintf("Failed to decode image data, %v", err))
 						return
 					}
@@ -84,6 +96,7 @@ func UpdateFunc() js.Func {
 					err = jpeg.Encode(jpg_wr, im, &opts)
 
 					if err != nil {
+						log.Println("SAD", 4)
 						reject.Invoke(fmt.Sprintf("Failed to decode image data as JPEG, %v", err))
 						return
 					}
@@ -91,21 +104,25 @@ func UpdateFunc() js.Func {
 					img_fh = jpg_r
 				}
 
+				log.Println("STEP", 5)
 				var buf bytes.Buffer
 				img_wr := bufio.NewWriter(&buf)
 
 				err = update.UpdateExif(img_fh, img_wr, exif_data)
 
 				if err != nil {
+					log.Println("SAD", 5)
 					reject.Invoke(fmt.Printf("Failed update EXIF properties, %v", err))
 					return
 				}
 
+				log.Println("STEP", 6)
 				img_wr.Flush()
 
 				b64_img = base64.StdEncoding.EncodeToString(buf.Bytes())
 				data_uri := fmt.Sprintf("data:image/jpeg;base64,%s", b64_img)
 
+				log.Println("RESOLVE OKAY", data_uri)
 				resolve.Invoke(data_uri)
 			}()
 
