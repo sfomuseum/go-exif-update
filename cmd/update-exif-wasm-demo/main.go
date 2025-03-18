dcafe60e966f08e121097c6765017677a64a50ff
Aaron Straup Cope <thisisaaronland@users.noreply.github.com>
2025-03-18 13:19:16 -0700

    Update to use `sfomuseum/js-sfomuseum-golang-wasm` (#15)
    * refactor update-exif-wasm-demo to use sfomuseum/js-sfomuseum-golang-wasm, stop using go-http-bootstrap
    
    * remove unused code
    
    ---------
    
    Co-authored-by: thisisaaronland <thisisaaronland@localhost>

--- cmd/update-exif-wasm-demo/main.go
+++ cmd/update-exif-wasm-demo/main.go
@@ -8,11 +8,9 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/aaronland/go-http-bootstrap"
 	"github.com/aaronland/go-http-server"
 	"github.com/sfomuseum/go-exif-update/www"
 	"github.com/sfomuseum/go-flags/flagset"
-	"github.com/sfomuseum/go-http-wasm/v2"
 )
 
 func main() {
@@ -20,8 +18,6 @@ func main() {
 	fs := flagset.NewFlagSet("server")
 
 	server_uri := fs.String("server-uri", "http://localhost:8080", "A valid aaronland/go-http-server URI.")
-
-	bootstrap_prefix := fs.String("bootstrap-prefix", "", "A relative path to append to all Bootstrap-related paths the server will listen for requests on.")
 
 	fs.Usage = func() {
 		fmt.Fprintf(os.Stderr, "HTTP server for demonstrating the use of the update_exif WebAssembly binary.\n\n")
@@ -49,29 +45,8 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	bootstrap_opts := bootstrap.DefaultBootstrapOptions()
-	bootstrap_opts.Prefix = *bootstrap_prefix
-
-	wasm_opts := wasm.DefaultWASMOptions()
-	
-	err = bootstrap.AppendAssetHandlers(mux, bootstrap_opts)
-
-	if err != nil {
-		log.Fatalf("Failed to append Bootstrap asset handlers, %v", err)
-	}
-
-	err = wasm.AppendAssetHandlers(mux, wasm_opts)
-
-	if err != nil {
-		log.Fatalf("Failed to append WASM asset handlers, %v", err)
-	}
-
 	http_fs := http.FS(www.FS)
 	fs_handler := http.FileServer(http_fs)
-
-	fs_handler = bootstrap.AppendResourcesHandler(fs_handler, bootstrap_opts)
-
-	fs_handler = wasm.AppendResourcesHandler(fs_handler, wasm_opts)
 
 	mux.Handle("/", fs_handler)
 
